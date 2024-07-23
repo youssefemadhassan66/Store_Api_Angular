782a1f0c59fc342dd9911d49c309dafc48871faa
youssef <youssefemadhassan66@gmail.com>
2024-07-23 15:03:54 +0300

    Intial Setup

---
+++ src/app/store-header/store-header.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { StoreHeaderComponent } from './store-header.component';
+
+describe('StoreHeaderComponent', () => {
+  let component: StoreHeaderComponent;
+  let fixture: ComponentFixture<StoreHeaderComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [StoreHeaderComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(StoreHeaderComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
